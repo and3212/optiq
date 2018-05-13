e942d7cb2358d07f72e9589c8e585d6d87ae95d4
and3212 <and3212@gmail.com>
2018-05-13 05:17:53 -0400

    Added some nicer header comments to my bash scripts

--- src/scripts/removeNewLine.sh
+++ src/scripts/removeNewLine.sh
@@ -1,7 +1,13 @@
 #!/bin/bash
-# Author: Liam Lawrence
-# Date: May 13, 2018
-# Removes an extra \n off of the step file which can cause crashes
- 
+#-----------------------------------------------------------------------------
+# File Name    : removeNewLine.sh
+# Description  : Removes an extra \n off of the step file which can sometimes cause a crash
+# Author       : Liam Lawrence
+# Created      : May 13, 2018
+#
+# Version      : 1.0
+# Last Updated : n/a
+#-----------------------------------------------------------------------------
+
 FILE="../res/step.txt"
 printf %s "$(< $FILE)" > $FILE
--- src/scripts/simulate.sh
+++ src/scripts/simulate.sh
@@ -1,7 +1,13 @@
 #!/bin/bash
-# Author: Liam Lawrence
-# Date: May 13, 2018
-# Runs the processing simulation with variables passed from the C++ code
+#-----------------------------------------------------------------------------
+# File Name    : simulate.sh
+# Description  : Runs the Processing simulation of the Optiq laser cutter
+# Author       : Liam Lawrence
+# Created      : May 13, 2018
+#
+# Version      : 1.0
+# Last Updated : n/a
+#-----------------------------------------------------------------------------
 
 SKETCHFOLDER="../../processing/simulation"
 BUILDFOLDER="$SKETCHFOLDER/build"
